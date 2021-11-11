5a7f0562c38a85b6df4d783e84feb1b28f8f3900
Tony Pennoyer <tonypennoyer@gmail.com>
2021-11-10 23:34:19 -0500

    simple cleaning and recoding sections

--- tony/ames_tony.py
+++ tony/ames_tony.py
@@ -33,10 +33,76 @@ categorical = ah[['MSZoning','Street','LotShape','LandContour','Utilities','LotC
 'RoofMatl','Exterior1st','Exterior2nd','MasVnrType','ExterQual','ExterCond','Foundation','BsmtQual','BsmtCond','BsmtExposure',
 'BsmtFinType1','BsmtFinType2','Heating','HeatingQC','CentralAir','Electrical','BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr',
 'KitchenAbvGr','KitchenQual','Functional','Fireplaces','GarageType','GarageFinish','GarageQual','GarageCond','PavedDrive','SaleType','SaleCondition']]
+#-----------------------------------------------------------------------------------------------#
 
-# for feature in categorical:
-#     print(ah[feature].value_counts())
-#     print('')
+#---------------------------------------- neighborhoods to section of town ---------------------#
+hoods = ah['Neighborhood']
+new_hoods = []
+
+for neigh in hoods :
+	if neigh == 'Blueste' :
+		new_hoods.append('SW')
+	elif neigh == 'Blmngtn' :
+		new_hoods.append('NO')
+	elif neigh == 'BrDale' :
+		new_hoods.append('NO')
+	elif neigh == 'BrkSide' :
+		new_hoods.append('DT')
+	elif neigh == 'ClearCr' :
+		new_hoods.append('NO')
+	elif neigh == 'CollgCr' :
+		new_hoods.append('SW')
+	elif neigh == 'Crawfor' :
+		new_hoods.append('SW')
+	elif neigh == 'Edwards' :
+		new_hoods.append('SW')
+	elif neigh == 'Gilbert' :
+		new_hoods.append('NO')
+	elif neigh == 'IDOTRR' :
+		new_hoods.append('DT')
+	elif neigh == 'MeadowV' :
+		new_hoods.append('SE')
+	elif neigh == 'Mitchel' :
+		new_hoods.append('SE')
+	elif neigh == 'NAmes' :
+		new_hoods.append('NO')
+	elif neigh == 'NoRidge' :
+		new_hoods.append('NW')
+	elif neigh == 'NPkVill' :
+		new_hoods.append('NO')
+	elif neigh == 'NridgHt' :
+		new_hoods.append('NW')
+	elif neigh == 'NWAmes' :
+		new_hoods.append('NO')
+	elif neigh == 'OldTown' :
+		new_hoods.append('DT')
+	elif neigh == 'SWISU' :
+		new_hoods.append('SW')
+	elif neigh == 'Sawyer' :
+		new_hoods.append('NW')
+	elif neigh == 'SawyerW' :
+		new_hoods.append('NW')
+	elif neigh == 'Somerst' :
+		new_hoods.append('NW')
+	elif neigh == 'StoneBr' :
+		new_hoods.append('NO')
+	elif neigh == 'Timber' :
+		new_hoods.append('SW')
+	elif neigh == 'Veenker' :
+		new_hoods.append('NW')
+	elif neigh == 'Greens' :
+		new_hoods.append('NW')
+	elif neigh == 'GrnHill' :
+		new_hoods.append('SO')
+	elif neigh == 'Landmrk' :
+		new_hoods.append('DT')
+	else : print('no match')
+
+ah['city_sec'] = new_hoods 
+#-----------------------------------------------------------------------------------------------#
+
+#---------------------------------------- central air to 1 and 0s ------------------------------#
+ah['CentralAir'] = ah['CentralAir'].map(lambda x: 0 if x == 'N' else 1)
 #-----------------------------------------------------------------------------------------------#
 
 
@@ -46,6 +112,11 @@ categorical = ah[['MSZoning','Street','LotShape','LandContour','Utilities','LotC
 
 
 
+# simpFeatures = ah[['']]
+
+# Neighborhood:
+# Central Air
+# Fireplaces
 
 
 
@@ -53,14 +124,3 @@ categorical = ah[['MSZoning','Street','LotShape','LandContour','Utilities','LotC
 
 
 
-
-
-
-
-
-
-
-
-
-# print('The columns with missingness are %s' %(ah.columns[missingCols]))
-# print('The columns with missingness are %s' %(ah.columns[missingCols]))
